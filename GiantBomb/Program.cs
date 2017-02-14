d1f1f632a7511ee6b84ba8ff253674c62de2dd68
Nick Lewis <nlewis2464@gmail.com>
2017-02-14 01:06:33 -0600

    Assignment 3 Submission: Pulls 100 images and labels with genre

--- GiantBomb/Program.cs
+++ GiantBomb/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * Created by SharpDevelop.
- * User: Nick
+ * User: Nick Lewis
  * Date: 2/8/2017
  * Time: 4:58 PM
  * 
@@ -12,96 +12,128 @@ using System.Linq;
 using System.Web.Script.Serialization;
 using System.IO;
 using System.Net;
+
 namespace GiantBomb
 {
-	class Program
+	/* 
+	 * Pulls NUM_SAMPLES cover art images from GiantBomb.com database of games
+	 * Stores those images locally for labeling
+	 */
+	class RawDataFetcher
 	{
+		// The number of raw images to request
+		private static int NUM_SAMPLES = 100;
+		
 		public static void Main(string[] args)
 		{
-			// GameIDs between 1 and 58285
 			int successCount = 0;
 			Random rnd = new Random();
-			while(successCount < 10){
+			// Download image samples until we've reached NUM_SAMPLES images.
+			while(successCount < NUM_SAMPLES){
+				// Select a random gameID
+				// GameIDs are between 1 and 58285 on GiantBomb.com (checked manually)
 				int randomID = rnd.Next(1, 58285);
 				Console.WriteLine("Checking gameID: "+ randomID);
+				
+				// Attempt to pull an image from GiantBomb, inc our successCount if success.
 				if (GiantBombPoller.downloadBoxArtByID("3030-"+randomID))
 				{
 					successCount++;
 				}
-		}
-			
+			}
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
-		
-		
-
 	}
 	
+	/*
+	 * Given a gameID, use GiantBomb's API to request information
+	 * about a game, including title, genre, and cover art.
+	 */
 	static class GiantBombPoller
 	{
 		// A private API key, obtained from GiantBomb.com to use their database
 		private static string API_KEY = "eaaf363258bb7689b65331f7e0a4ae3dcbaf975c";
-			
+		
+		/*
+		 * Given a gameID, check if it contains the attributes we need
+		 * (i.e. the ID is valid in the database, and it corresponds to
+		 * at least 1 genre and 1 piece of cover art)
+		 * Return if the gameID yielded data we need
+		 */
 		public static bool downloadBoxArtByID(string gameID)
 		{
 			// Create a webClient to make requests to GiantBomb
 			// Used: http://stackoverflow.com/questions/5566942/how-to-get-a-json-string-from-url
-			//		 to debug user-agent problems			
+			//		 to debug user-agent problems
 			System.Net.WebClient wb = new System.Net.WebClient();
 			wb.Headers.Add("User-Agent: VanderbiltUniversityCS4269");
 
-			// Request a JSON string that contains image and name data for a given gameID.
+			// Request a JSON string from GiantBomb that contains image, 
+			// genre and name data for a given gameID.
 			// Note: a valid API key is required
-			string gameRequest = "http://www.giantbomb.com/api/game/" + gameID + 
-				"/?api_key=" + API_KEY + "&format=json&field_list=game,image,name";
+			string gameRequest = "http://www.giantbomb.com/api/game/" + gameID +
+				"/?api_key=" + API_KEY + "&format=json&field_list=game,image,name,genres";
 			try{
 				string JSONResult = wb.DownloadString(gameRequest);
-				Console.WriteLine(JSONResult);
 				
-			// Deserialize the JSON so we can navigate it.
-			// Used: http://stackoverflow.com/questions/3142495/deserialize-json-into-c-sharp-dynamic-object/9326146#9326146
-			//		 for reference
-			JavaScriptSerializer serializer =  new JavaScriptSerializer();
-			dynamic item = serializer.Deserialize<object>(JSONResult);
-			if(item["error"] != "OK" || item["results"]["image"] == null)
-			{
-				return false;
+				// Deserialize the JSON so we can navigate it.
+				// Used: http://stackoverflow.com/questions/3142495/deserialize-json-into-c-sharp-dynamic-object/9326146#9326146
+				//		 for reference
+				JavaScriptSerializer serializer =  new JavaScriptSerializer();
+				dynamic item = serializer.Deserialize<object>(JSONResult);
+				// If the database returns or error, no images exist, or no genres exist, return failure.
+				if(item["error"] != "OK" || !item["results"].ContainsKey("image") || 
+				   item["results"]["image"] == null || !item["results"].ContainsKey("genres"))
+				{
+					return false;
+				}
+				// Get the URL for the boxArt, as well as the gameTitle and genre
+				string boxArtURL = item["results"]["image"]["super_url"];
+				string gameGenre = item["results"]["genres"][0]["name"];
+				string gameTitle = item["results"]["name"];
+				
+				// Pinball is a weird case where its cover art doesn't really follow
+				// the other genres. Exclude Pinball samples.
+				if (gameGenre == "Pinball")
+					return false;
+				// Write the information to a text file (ID, suggested genre) for further labeling.
+				// TODO: Create environment safe write path
+				File.AppendAllText("D:\\Nick\\Pictures\\Game_BoxArt\\Genres.txt", 
+				                   gameID + " " + /*gameTitle + " " + */gameGenre + Environment.NewLine);
+				
+				// Download the file as JPG
+				// TODO: Create environment safe download path
+				string download_path =  Path.Combine("D:\\Nick\\Pictures\\Game_BoxArt\\", gameID+".jpg");
+				
+				// Try to download the image from the database, return the result.
+				return downloadImage(boxArtURL, download_path);
 			}
-			// Get the URL for the boxArt, as well as the gameTitle
-			string boxArtURL = item["results"]["image"]["thumb_url"];
-			string gameTitle = item["results"]["name"];
-			
-			// The filepath can not contain special characters, parse them out
-			// TODO: Parse out additional special characters. Refactor into separate method
-			string safeGameTitle = gameTitle.Replace(":", "");
-			// Download the file as JPG
-			// TODO: Create environment safe download path
-			string download_path =  Path.Combine("D:\\Nick\\Pictures\\Game_BoxArt\\", safeGameTitle+".jpg");
-			
-			return downloadImage(boxArtURL, download_path);
-			}
+			// Catch failure polling GiantBomb servers
 			catch(WebException wexc){
 				Console.Write("Error requesting game data: " + gameID);
 				return false;
 			}
-			
 		}
 		
+		/*
+		 * Given a url and a local filepath, download an image
+		 * Return if download succeeded
+		 */
 		public static bool downloadImage(string imageUrl, string fileName)
 		{
 			try{
+				// Create a new WebClient and attempt to download the image
 				System.Net.WebClient wb = new System.Net.WebClient();
 				wb.Headers.Add("User-Agent: VanderbiltUniversityCS4269");
 				wb.DownloadFile(imageUrl, fileName);
 			}
+			// Catch failure downloading from GiantBomb
 			catch(WebException wexc){
 				Console.WriteLine("Error downloading Image");
 				return false;
 			}
 			return true;
 		}
-
 	}
-
 }
